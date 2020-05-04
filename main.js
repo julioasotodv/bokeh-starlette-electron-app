c2e2e966d7c5e0735958daa6405d466d0b8794a9
Julio <julio@esbet.es>
2020-05-04 02:37:43 +0200

    Added fix to ensure different ports are always used for Uvicorn and Bokeh Server

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ portfinder.getPort(function(err, port) {
   uvicornPort = '' + port
 });
 
+portfinder.basePort = portfinder.basePort + 1
 portfinder.getPort(function(err, port) {
   bokehPort = '' + port
 });
